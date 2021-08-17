5a672f7d7301ee6b8d0af72593fda3d702f489cc
Guilheeeerme <guilherme_moura.10222@outlook.com>
2021-08-16 23:16:58 -0300

    example

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,20 @@
-const Router = require('koa-router')
+const Router = require("koa-router");
 
 // middlewares
-const error = require('./middlewares/error')
-const authenticated = require('./middlewares/auth')
+const error = require("./middlewares/error");
+const authenticated = require("./middlewares/auth");
 
 // handlers
-const users = require('./handlers/users')
-const auth = require('./handlers/auth')
+const users = require("./handlers/users");
+const auth = require("./handlers/auth");
 
-const router = new Router()
+const router = new Router();
 
-router.use(error)
+router.use(error);
 
-router.get('/users', users.getAllUsers)
-router.post('/users', authenticated, users.createUser)
+router.get("/users", authenticated, users.getAllUsers);
+router.post("/users", authenticated, users.createUser);
 
-router.post('/auth', auth.authenticate)
+router.post("/auth", auth.authenticate);
 
-module.exports = router
+module.exports = router;
--- src/services/token.js
+++ src/services/token.js
@@ -1,8 +1,21 @@
-const sign = _ => 'Generated token'
+const jwt = require("jsonwebtoken");
+const { crypto: config } = require("../../config");
 
-const verify = _ => new Promise((resolve, reject) => resolve(true))
+const signOptions = {
+  algorithm: "RS256",
+  expiresIn: "1w",
+};
+
+const sign = (payload) => jwt.sign(payload, config.jwt.privateKey, signOptions);
+
+const verify = (token) =>
+  new Promise((resolve, reject) =>
+    jwt.verify(token, config.jwt.publicKey, (error, data) =>
+      error ? reject() : resolve(data)
+    )
+  );
 
 module.exports = {
   sign,
   verify,
-}
+};
